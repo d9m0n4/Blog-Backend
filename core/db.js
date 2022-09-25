cb4ecfe7807ea1fec781921475266d25c981d8f2
d9m0n <c2h8e9d4@gmail.com>
2022-09-25 23:08:25 +0300

    5

--- core/db.js
+++ core/db.js
@@ -1,32 +1,32 @@
-// import { Sequelize } from 'sequelize';
-// import dotenv from 'dotenv';
-// dotenv.config();
+import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
+dotenv.config();
 
-// const sequlize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-//   dialect: 'postgres',
-//   host: process.env.DB_HOST,
-//   port: process.env.DB_PORT,
-// });
+const sequlize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  dialect: 'postgres',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+});
 
-// import pkg from 'pg'
+import pkg from 'pg';
 
-// const {Client} = pkg
+const { Client } = pkg;
 
-// const client = new Client({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: {
-//     rejectUnauthorized: false,
-//   },
-// });
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
-// client.connect();
+client.connect();
 
-// client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-//   if (err) throw err;
-//   for (let row of res.rows) {
-//     console.log(JSON.stringify(row));
-//   }
-//   client.end();
-// });
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
 
-// export default sequlize;
+export default sequlize;
--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 import express, { json } from 'express';
 import cors from 'cors';
 import Routes from './Routes/index.js';
+import sequlize from './core/db.js';
 import dotenv from 'dotenv';
 import ErrorHandling from './middleware/ErrorHandling.js';
 import fileUpload from 'express-fileupload';
 import path from 'path';
-import pkg from 'pg';
-
 import * as models from './models/models.js';
 import cookieParser from 'cookie-parser';
 
-const { Client } = pkg;
 const __dirname = path.resolve(path.dirname(''));
 
 dotenv.config();
@@ -31,26 +29,10 @@ app.use('/api', Routes());
 
 app.use(ErrorHandling);
 
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
-
 const start = async () => {
   try {
-    // await sequlize.authenticate();
-    // await sequlize.sync();
-    client.connect();
-
-    client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-      if (err) throw err;
-      for (let row of res.rows) {
-        console.log(JSON.stringify(row));
-      }
-      client.end();
-    });
+    await sequlize.authenticate();
+    await sequlize.sync();
     app.listen(process.env.PORT, (err) => {
       if (err) {
         return console.log(err);
