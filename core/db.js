3d5e3859fe22f568b598d85ac74b93c34bd9bdcd
Дмитрий <59685224+d9m0n4@users.noreply.github.com>
2022-09-25 23:26:52 +0300

    Update db.js

--- core/db.js
+++ core/db.js
@@ -1,16 +1,14 @@
 import { Sequelize } from 'sequelize';
 import pkg from 'pg';
-const { Client } = pkg;
 import dotenv from 'dotenv';
 dotenv.config();
+const { Client } = pkg;
 
 // const sequlize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
 //   dialect: 'postgres',
 //   host: process.env.DB_HOST,
 //   port: process.env.DB_PORT,
 // });
-
-
 
 const sequlize = new Client({
   connectionString: process.env.DATABASE_URL,
@@ -19,14 +17,14 @@ const sequlize = new Client({
   },
 });
 
-client.connect();
+sequlize.connect();
 
-client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+sequlize.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
   if (err) throw err;
   for (let row of res.rows) {
     console.log(JSON.stringify(row));
   }
-  client.end();
+  sequlize.end();
 });
 
 export default sequlize;
