57257342aaa1f29cbf73527458504ac1f6bcd0dd
Дмитрий <59685224+d9m0n4@users.noreply.github.com>
2022-09-25 22:47:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 import express, { json } from 'express';
 import cors from 'cors';
 import Routes from './Routes/index.js';
-import sequlize from './core/db.js';
 import dotenv from 'dotenv';
 import ErrorHandling from './middleware/ErrorHandling.js';
 import fileUpload from 'express-fileupload';
 import path from 'path';
+import pkg from 'pg';
+
 import * as models from './models/models.js';
 import cookieParser from 'cookie-parser';
 
+const { Client } = pkg;
 const __dirname = path.resolve(path.dirname(''));
 
 dotenv.config();
@@ -29,10 +31,26 @@ app.use('/api', Routes());
 
 app.use(ErrorHandling);
 
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
 const start = async () => {
   try {
-    await sequlize.authenticate();
-    await sequlize.sync();
+    // await sequlize.authenticate();
+    // await sequlize.sync();
+    client.connect();
+
+    client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+      if (err) throw err;
+      for (let row of res.rows) {
+        console.log(JSON.stringify(row));
+      }
+      client.end();
+    });
     app.listen(process.env.PORT, (err) => {
       if (err) {
         return console.log(err);
