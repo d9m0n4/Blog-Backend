69f73d614f662583e084e4596f53fe6fc03a7746
d9m0n <c2h8e9d4@gmail.com>
2022-09-25 22:32:57 +0300

    2

--- core/db.js
+++ core/db.js
@@ -1,11 +1,30 @@
-import { Sequelize } from 'sequelize';
-import dotenv from 'dotenv';
-dotenv.config();
+// import { Sequelize } from 'sequelize';
+// import dotenv from 'dotenv';
+// dotenv.config();
 
-const sequlize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  dialect: 'postgres',
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+// const sequlize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+//   dialect: 'postgres',
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+// });
+
+import { Client } from 'pg';
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
 });
 
 export default sequlize;
