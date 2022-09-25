09707e7a17355148447055c2a14c17619c641f57
Дмитрий <59685224+d9m0n4@users.noreply.github.com>
2022-09-25 23:24:43 +0300

    Update db.js

--- core/db.js
+++ core/db.js
@@ -1,18 +1,18 @@
 import { Sequelize } from 'sequelize';
+import pkg from 'pg';
+const { Client } = pkg;
 import dotenv from 'dotenv';
 dotenv.config();
 
-const sequlize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  dialect: 'postgres',
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-});
+// const sequlize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+//   dialect: 'postgres',
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+// });
 
-import pkg from 'pg';
 
-const { Client } = pkg;
 
-const client = new Client({
+const sequlize = new Client({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
