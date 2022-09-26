ea1adfee781ada83d1dbe4558a50190ac288438b
Дмитрий <59685224+d9m0n4@users.noreply.github.com>
2022-09-26 21:19:03 +0300

    Update authHandler.js

--- middleware/authHandler.js
+++ middleware/authHandler.js
@@ -4,17 +4,17 @@ import TokenService from '../service/token.js';
 export default function (req, res, next) {
   try {
     const authorizationHeader = req.headers.authorization;
-
+    console.log('authHr', authorizationHeader)
     if (!authorizationHeader) {
       return next(ApiError.unauthorized('Пользователь не авторизован'));
     }
     const accessToken = authorizationHeader.split(' ')[1];
-
+   console.log('access', accessToken)
     if (!accessToken) {
       return next(ApiError.unauthorized('Пользователь не авторизован'));
     }
     const userData = TokenService.validateAccessToken(accessToken);
-
+console.log('ud', userData)
     if (!userData) {
       return next(ApiError.unauthorized('Пользователь не авторизован'));
     }
